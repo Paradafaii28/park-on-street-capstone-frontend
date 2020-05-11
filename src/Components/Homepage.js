807db902c0607ebfed27434bb0c2a17d1163b060
github username <Paradafaii28@gmail.com>
2020-05-11 14:43:48 -0600

    frontpage design

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import './Homepage.css'
-import BgHomePage from '../Photo/bg1.png'
+import BgHomePage from '../Photo/bg3.png'
 
 
 export default class Homepage extends Component {
     render() {
         return (
-            <div>
+            <section>
                 <div className='bg-homepage-container'>
+                    <div className='bg-text-homepage'>
+                       <h1>PARK - ON - STREET</h1>
+                       <h5>find a perfect spot to park.</h5>
+                    </div>
                     {<img src={BgHomePage}/>}
                 </div>
-            </div>
+            </section>
         )
     }
 }
